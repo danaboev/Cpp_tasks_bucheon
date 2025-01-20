35789dd5ccb78bd6e51ad326eaa32a795937b6e4
jahon <danaboev.jf@gmail.com>
2025-01-20 10:37:06 +0500

    poli_task

---
+++ A/polimor_full_task.cpp
@@ -0,0 +1,37 @@
+
+class Vehicle {
+public:
+    virtual void start() = 0; // Чистая виртуальная функция
+    virtual void stop() = 0;  // Чистая виртуальная функция
+    virtual void displayInfo() = 0; // Чистая виртуальная функция
+};
+
+class Car : public Vehicle {
+private:
+    int numberOfDoors;
+public:
+    Car(int doors);
+    void start() override;
+    void stop() override;
+    void displayInfo() override;
+};
+
+class Bike : public Vehicle {
+private:
+    string type;
+public:
+    Bike(string bikeType);
+    void start() override;
+    void stop() override;
+    void displayInfo() override;
+};
+
+class Garage {
+private:
+    vector<Vehicle*> vehicles;
+public:
+    void addVehicle(Vehicle* vehicle);
+    void startAll();
+    void stopAll();
+    void displayAll();
+};
